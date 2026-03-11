56ba0606ffb3380a34cd0bb5fcb4c6780b0a9ced
YongVuthivathnakk <vuthivathnakkyong@gmail.com>
2026-03-11 21:59:54 +0700

    added checkout page

---
+++ app/(app)/checkout/page.tsx
@@ -0,0 +1,10 @@
+import { CheckoutClient } from "./CheckoutClient";
+
+export const metadata = {
+  title: "Checkout - Samki",
+  desciption: "Complete your purchase",
+};
+
+export default function CheckoutPage() {
+  return <CheckoutClient />;
+}
